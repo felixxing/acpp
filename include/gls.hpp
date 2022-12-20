f17bbc716f6979b07a26469f5023fd0c7bf5e26f
Felix Xing <101950667+felixxing@users.noreply.github.com>
2022-12-19 20:54:40 -0500

    gls header ready

--- include/gls.hpp
+++ include/gls.hpp
@@ -1,11 +1,25 @@
 #ifndef GLS_HPP
 #define GLS_HPP
 
+// STL
 #include <iostream>
 #include <memory>
 #include <string>
 
+// OpenGl
+#define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include "glad/glad.hpp"
+
+// GLM
+#define GLM_FORCE_INLINE
+#define GLM_FORCE_XYZW_ONLY
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+//fmt 
+#include <fmt/core.h>
 
 #endif // GLS_HPP
